815b5fcde17ae0937424166387d63f9ec9458d80
Fillsogood <jdd05273@naver.com>
2025-03-20 11:56:48 +0900

    feat-보호자 강아지 getInfo 기능 구현

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import client.Dog;
 import client.Owner;
 import server.Server;
 
@@ -7,7 +8,7 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    Server server = new Server();
+//    Server server = new Server();
 
     System.out.println("=== 멍훈련소 시스템 ===");
     System.out.println("1. 보호자로 로그인");
@@ -25,14 +26,15 @@ public class Main {
       System.out.print("강아지 나이 입력: ");
       int age = scanner.nextInt();
       scanner.nextLine();
-//      Owner owner = new Owner(ownerName, breed, age, server);
+      Owner owner = new Owner(ownerName, age,breed);
 //      owner.connectServer();
 
       while (true) {
         System.out.println("\n=== 보호자 모드 ===");
         System.out.println("1. 훈련사명 리스트(방 탐색)");
         System.out.println("2. 훈련 기록 조회 (/history)");
-        System.out.println("3. 나가기");
+        System.out.println("3. 내정보 확인");
+        System.out.println("4. 나가기");
         System.out.print("✏️ 선택: ");
         int choice = scanner.nextInt();
         scanner.nextLine();
@@ -64,6 +66,9 @@ public class Main {
 //            owner.viewTrainingHistory();
             break;
           case 3:
+            System.out.println(owner.getInfo());
+            break;
+          case 4:
             System.out.println("나가기");
             return;
           default:
--- src/client/Dog.java
+++ src/client/Dog.java
@@ -1,4 +1,16 @@
 package client;
 
-public class Dog {
+public class Dog{
+  public String name;
+  private final int age;
+  private final String breed;
+
+  public Dog(String name, int age, String breed) {
+    this.age = age;
+    this.breed = breed;
+  }
+
+  public String getInfo() {
+    return "강아지 나이: " + age + ", 강아지 크기: " + breed;
+  }
 }
