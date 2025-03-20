2f0a003a3d4813f81a605893d459e81070e4aa95
eogus623 <eogus623@gmail.com>
2025-03-20 16:41:04 +0900

    feat-클라이언트 접

--- src/server/OwnerHandler.java
+++ src/server/OwnerHandler.java
@@ -1,12 +1,8 @@
 package server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
 
-public class OwnerHandler implements Runnable {
+
+public class OwnerHandler implements Runnable{
 
 
   @Override
