1c34d2d4433648a1d28ab6738f81f9c4f7585c2e
eogus623 <eogus623@gmail.com>
2025-03-20 14:34:23 +0900

    chore-ìfeat - 훈련 방 생성

---
+++ src/server/OwnerHandler.java
@@ -0,0 +1,19 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class OwnerHandler implements Runnable {
+
+
+  @Override
+  public void run() {
+
+  }
+
+  public void sendMessage(String message) {
+  }
+}
